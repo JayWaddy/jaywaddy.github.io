3f4291cb79068cccadd8f79bef77e7e9acdcadbd
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2020-01-06 21:47:37 -0500

    parallax bg
    - parallax is fully functional
    - images need to repeat seamlessly

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    parallax();
+};
+
+function parallax() {
+    const bg = document.querySelectorAll('.parallax');
+
+    window.addEventListener('scroll', () => {
+        let offset = window.pageYOffset * 0.8;
+
+        // offset both .parallax divs
+        for(let i = 0; i < bg.length; i++) {
+            bg[i].style.backgroundPositionY = offset + 'px';
+        }
+    });
+}
