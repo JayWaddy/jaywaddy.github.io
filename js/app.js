5539d297d9c2088bdb707e47e72612b26e564bc4
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2020-01-08 18:28:21 -0500

    html structure
    - base html elements all in place
    - base styles comlpete (including grid)

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function parallax() {
     window.addEventListener('scroll', () => {
         let offset = window.pageYOffset * 0.8;
 
-        // offset both .parallax divs
+        // offset both .parallax elements
         for(let i = 0; i < bg.length; i++) {
             bg[i].style.backgroundPositionY = offset + 'px';
         }
