17cf1460b7715625fe9d2ccb4d41f450daec8a5b
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2020-01-09 12:28:03 -0500

    navigation scroll effect
    smooth page jump implemented.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 window.onload = () => {
     parallax();
     rollText();
+    navLinks();
 };
 
+function navLinks() {
+    const proj = document.querySelector('#navbar nav').firstElementChild;
+    const about = proj.nextElementSibling;
+    proj.addEventListener('click', () => scroll('#projects', 600));
+    about.addEventListener('click', () => scroll('#about', 600));
+}
+
+// parallax background effect
 function parallax() {
     const bg = document.querySelectorAll('.parallax');
 
@@ -16,16 +25,16 @@ function parallax() {
     });
 }
 
+// navbar rolling title effect
 function rollText() {
     const span = document.querySelector('.roll');
-    const list = [
+    const arr = [
         'UX/UI Designer',
         'Avid Wireframer-er',
         'Front-End Developer',
         'Stack Overflow Regular...',
-        'Ctrl + Z Spammer'
+        'Ctrl+Z Spammer'
     ];
-
     let counter = 0;
 
     // initial iterate
@@ -45,11 +54,35 @@ function rollText() {
 
     // list iteration
     function roll(num) {
-        if (counter < list.length - 1) {
+        if (counter < arr.length - 1) {
             counter += 1;
         } else {
             counter = 0;
         }
-        return list[num];
+        return arr[num];
     }
+}
+
+// smooth navigation scroll effect
+function scroll(tar, dur) {
+    let target = document.querySelector(tar);
+    let targetPos = target.getBoundingClientRect().top - 59;
+    let startPos = window.pageYOffset;
+    let startTime = null;
+
+    function jump(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        let elapsedTime = currentTime - startTime;
+        let run = ease(elapsedTime, startPos, targetPos, dur);
+        window.scrollTo(0, run);
+        if(elapsedTime < dur) requestAnimationFrame(jump);
+    }
+
+    // easing function from http://gizma.com/easing
+    function ease(t, b, c, d) {
+        if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+        return -c/2 * ((t-=2)*t*t*t - 2) + b;
+    };
+
+    requestAnimationFrame(jump);
 }
