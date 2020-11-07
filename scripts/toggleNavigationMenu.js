e62f3631e8d84dd5730fc4a2635f260fa59c47c0
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2020-11-06 19:27:37 -0500

    Menu animation & layout simplification

--- scripts/toggleNavigationMenu.js
+++ scripts/toggleNavigationMenu.js
@@ -1,23 +1,21 @@
 (function toggleNavigationMenu() {
-    const NAVIGATION_BACKGROUND = document.querySelector("nav .navigation-background"),
-          NAVIGATION_LINKS = document.querySelector("nav ul li"),
-          MENU_ICON = document.querySelector(".menu-icon");
+    const navigationBackground = document.querySelector("nav .navigation-background"),
+          navigationLinks = document.querySelector("nav ul"),
+          menuIcon = document.querySelector(".menu-icon"),
+          mainContent = document.querySelector(".main-content");
     let menuIsToggled = false;
 
-    MENU_ICON.addEventListener("click", function () {
+    menuIcon.addEventListener("click", function () {
         menuIsToggled = !menuIsToggled;
         menuIsToggled ? openMenu() : closeMenu();
+        ;
     });
 
     function openMenu() {
-        // 1. set navigation links to "visible"
-        // 2. add animation class(es) to menu icon, background, and navigation links
-        console.log("menu is open");
+        mainContent.style.transform = "scale(0.7)";
     }
 
     function closeMenu() {
-        // 1. animate links, background, and menu icon out of view
-        // 2. set navigation links to display: none
-        console.log("menu is closed");
+        mainContent.style.transform = "scale(1)";
     }
 })();
