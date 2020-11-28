cde2795102d13f86989054b62624841eb2c1a747
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2020-11-28 16:04:22 -0500

    Work page transitions

--- scripts/handlePageRedirect.js
+++ scripts/handlePageRedirect.js
@@ -6,12 +6,6 @@ let currentURL = "",
     delay = 50;
 
 window.onload = animateContentIn();
-// window.addEventListener("wheel", () => {
-//     if (window.location.pathname === "/") {
-//         animateContentOut();
-//         setTimeout(() => window.location.pathname = "/work.html", 500);
-//     }
-// });
 
 for (let link of links) {
     link.addEventListener("click", function(event){
@@ -26,12 +20,31 @@ for (let link of links) {
 }
 
 function animateContentIn() {
+    const background = document.querySelectorAll(".work-entry-background"),
+          details = document.querySelectorAll(".work-entry-transition");
+
+    // Global transitiions
     for (let i = 0; i < content.length; i++) {
         content[i].classList.add("transition--enter");
         setTimeout(() => content[i].classList.add("transition--enter-active"), i * delay);
         setTimeout(() => {
             content[i].classList.remove("transition--enter", "transition--enter-active");
             content[i].classList.add("transition--enter-done");
+        }, 1000);
+    }
+
+    // Work entry transtitions
+    for (let i = 0; i < background.length; i++) {
+        background[i].classList.add("work-entry-background--enter");
+        setTimeout(() => {
+            setTimeout(() => background[i].classList.add("work-entry-background--enter-active"), i * 100);
+        }, 600);
+    }
+
+    for (let i = 0; i < details.length; i++) {
+        details[i].classList.add("work-entry-transition--enter");
+        setTimeout(() => {
+            setTimeout(() => details[i].classList.add("work-entry-transition--enter-active"), i * 100);
         }, 1000);
     }
 }
